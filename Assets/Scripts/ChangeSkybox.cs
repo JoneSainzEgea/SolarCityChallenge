eb9eaba09ef63567a323f4b6a767315118900557
Jonetxu10 <jonesainzegea@gmail.com>
2026-04-18 13:39:10 +0200

    SkyboxChange

---
+++ Assets/Scripts/ChangeSkybox.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSkybox : MonoBehaviour
+{
+    [SerializeField] private Material skyboxDay;
+    [SerializeField] private Material skyboxNight;
+    [SerializeField] private float transitionDuration = 2f;
+
+    private bool isDay = true;
+    private Coroutine transitionCoroutine;
+
+    public void OnChangeSkybox()
+    {
+        if (transitionCoroutine != null)
+            StopCoroutine(transitionCoroutine);
+
+        if (isDay)
+            transitionCoroutine = StartCoroutine(FadeSkybox(skyboxNight));
+        else
+            transitionCoroutine = StartCoroutine(FadeSkybox(skyboxDay));
+
+        isDay = !isDay;
+    }
+
+    IEnumerator FadeSkybox(Material newSkybox)
+    {
+        float t = 0;
+
+        while (t < transitionDuration/2)
+        {
+            RenderSettings.skybox.SetFloat("_Exposure", 1 - t);
+            t += Time.deltaTime;
+            yield return null;
+        }
+
+        RenderSettings.skybox = newSkybox;
+
+        t = 0;
+        while (t < transitionDuration/2)
+        {
+            RenderSettings.skybox.SetFloat("_Exposure", t);
+            t += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
